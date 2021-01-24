59f77df0330fca36a829c47b0d6002a4b42cee64
Alicx Kamoun <alicx.kamoun@hotmail.fr>
2021-01-24 22:54:44 +0100

    added md5 check

--- Piplilne.py
+++ Piplilne.py
@@ -1,15 +1,26 @@
 import wget
 import csv
 import os
+import hashlib
 import subprocess as sp
 path = os.getcwd()
 
 def download_sample(url, md5, dirName):
-    print("Téléchargement du fichier\n", dirName)
-    nameFastQ = "./" + dirName + "/" + url.rsplit('/', 1)[-1]
-    sp.call(['wget', '-O', nameFastQ, url])
+    print("Téléchargement du fichier", dirName)
+    fastqName = url.rsplit('/', 1)[-1]
+    pathFastQ = path + "/" + dirName + "/" + fastqName
 
-    #md5 à check plus tard
+    #Downloading file then moving it
+    if (not os.path.isfile(pathFastQ)): 
+        sp.call(['wget', '-O', fastqName, url])
+        currentPath = path + "/" + fastqName
+        os.replace(currentPath, pathFastQ)
+    
+    #Checking md5
+    currentMd5 = hashlib.md5(open(pathFastQ,'rb').read()).hexdigest()
+    if currentMd5 == md5: print("md5 correct \n")
+    else: print("md5 incorrect \n")
+        
 
 
 lien = input("Tapez le lien du projet: \n")
@@ -18,7 +29,7 @@ print(project)
 print('Début du téléchargement du fichier TSV') #On télécharge le fichier TSV en se basant sur le code du projet
 url = 'https://www.ebi.ac.uk/ena/portal/api/filereport?accession='+ project +'&result=read_run&fields=fastq_md5,fastq_ftp,sample_title&format=tsv&download=true'
 nameTsv = "FILE_"+project+".tsv"
-sp.call(['wget', '-O', nameTsv, url])
+#sp.call(['wget', '-O', nameTsv, url])
 print("\n TSV file downloaded\n")
 
 
@@ -32,7 +43,6 @@ for row in read_tsv:
   #print(row)
   pathFile = path + "/files/" + row[4]
   if not os.path.isdir(pathFile): os.mkdir(pathFile)
-  print('\n Téléchargement du fichier zip\n')
   if row[2]:
       if ';' in row[3]:
           tmpFastQ = row[3].split(';')
